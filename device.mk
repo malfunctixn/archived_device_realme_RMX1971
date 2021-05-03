88a0033b0c12777412b12000ef3ddbb5e9bf91e5
malfunctixn <malfunctixn@gmail.com>
2021-05-03 13:04:23 +0530

    introduce evolution-overlays

--- device.mk
+++ device.mk
@@ -234,7 +234,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-evolution \
     $(LOCAL_PATH)/overlay
 
 # Permissions
---
+++ evolution_RMX1971.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifer: Apache-2.0
+#
+
+# Inherit some common evolution stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+
+# Inherit from RMX1971 device.
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Official-ify
+EVO_BUILD_TYPE := Unofficial
+TARGET_USES_BLUR := true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_DEVICE := RMX1971
+PRODUCT_NAME := evolution_RMX1971
+PRODUCT_BRAND := Realme
+PRODUCT_MODEL := Realme 5 Pro
+PRODUCT_MANUFACTURER := Realme
+
+PRODUCT_GMS_CLIENTID_BASE := android-realme
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210405.005 7181113 release-keys"
+
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="RMX1971" \
+    TARGET_DEVICE="RMX1971"
